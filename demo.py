4018187f131bc933f44981c3739f1ab747024944
LiuPhyMan <liu.jinbao@outlook.com>
2018-09-26 20:57:55 +0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 16:56 2018/9/26
+
+@author:    Liu Jinbao
+@mail:      liu.jinbao@outlook.com
+@project:   DemoPyQt5
+@IDE:       PyCharm
+"""
+
+from PyQt5 import QtWidgets as QW
+
+
+class SpecData(object):
+
+    def __init__(self):
+        self.wavelength = None
+        self.intensity = None
+
+    def
