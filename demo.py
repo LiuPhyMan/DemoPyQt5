95282659afddee913876fb17955a1aae9017d383
LiuPhyMan <liu.jinbao@outlook.com>
2018-09-28 19:47:54 +0800

    Add multi-lines plot, highlight function, some slots

--- demo.py
+++ demo.py
@@ -10,12 +10,24 @@ Created on 16:56 2018/9/26
 """
 import sys
 import re
+import numpy as np
 from PyQt5 import QtWidgets as QW
 from PyQt5.QtGui import QIcon, QPixmap, QColor, QFont, QCursor
 from PyQt5.QtCore import Qt, QSize, pyqtSignal
 from matplotlib.figure import Figure
+from matplotlib.widgets import Cursor
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
+
+
+class BetterButton(QW.QPushButton):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setCursor(QCursor(Qt.PointingHandCursor))
+        _font = QFont('Ubuntu', 11)
+        self.setFont(_font)
+        self.setStyleSheet(':hover {background-color: #87CEFA ;}')
 
 
 class PlotCanvas(FigureCanvas):
@@ -46,6 +58,7 @@ class QPlot(QW.QWidget):
 
 class ReadFileQWidget(QW.QWidget):
     pathChanged = pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.path = ""
@@ -53,9 +66,12 @@ class ReadFileQWidget(QW.QWidget):
         self._entry.setEnabled(True)
         self._entry.setMinimumWidth(600)
         self._entry.setCursor(QCursor(Qt.IBeamCursor))
-        self._browse = QW.QPushButton('Browse')
+        self._entry.setFont(QFont("Ubuntu", 10))
+        self._browse = BetterButton("Browse")
         self._browse.setCursor(QCursor(Qt.PointingHandCursor))
         self._set_layout()
+        self._set_connect()
+        self._set_slot()
 
     def _set_layout(self):
         _layout = QW.QHBoxLayout()
@@ -63,35 +79,44 @@ class ReadFileQWidget(QW.QWidget):
         _layout.addWidget(self._browse)
         _layout.addStretch(1)
         self.setLayout(_layout)
-        self._set_connect()
 
     def _set_connect(self):
         self._browse.clicked.connect(self._browse_callback)
 
     def _browse_callback(self):
+        print('button_pressed')
         _path = QW.QFileDialog.getOpenFileName(caption='Open File',
                                                filter="spec file (*.asc)")[0]
-        shorten_path = re.fullmatch(r".*(/[^/]+.asc)", _path).groups()[0]
+        shorten_path = re.fullmatch(r".*(/[^/]+/[^/]+/[^/]+.asc)", _path).groups()[0]
         self.path = _path
         self._entry.setText(shorten_path)
 
     def _set_slot(self):
-        self._entry.chan
+        def slot_emit():
+            print('emit')
+            self.pathChanged.emit()
+
+        self._entry.textChanged.connect(slot_emit)
 
 
 class SpectraPlot(QPlot):
 
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super().__init__(parent, figsize=(12, 6))
         self.axes = self.figure.add_subplot(111)
         self.axes.set_xlabel('Wavelength [nm]')
         self.axes.set_ylabel("Intensity [a.u.]")
+        self.axes.grid(linestyle='--')
+        self._cursor = Cursor(self.axes, color='green', linewidth=.5)
+        self.figure.tight_layout()
         self._spec_data = []
         self._lines = []
+        # self._set_connect()
 
     def add_spec(self, wavelength, intensity):
+        print('add_spec')
         self._spec_data.append(SpecData(wavelength, intensity))
-        _ln, = self.axes.plot(wavelength, intensity, linewidth=.5, marker='.')
+        _ln, = self.axes.plot(wavelength, intensity, linewidth=.5, marker='.', alpha=0.5)
         self._lines.append(_ln)
         self.canvas_draw()
 
@@ -100,10 +125,28 @@ class SpectraPlot(QPlot):
         self._lines.pop(index_to_delete)
         self.canvas_draw()
 
+    def highlight_line(self, index_to_highlight):
+        for _i, _ln in enumerate(self._lines):
+            if _i == index_to_highlight:
+                _ln.set_linewidth(1.5)
+                _ln.set_alpha(1)
+            else:
+                _ln.set_linewidth(.5)
+                _ln.set_alpha(.5)
+        self.canvas_draw()
+
     def canvas_draw(self):
         self.canvas.draw()
     # def plot_spectra(self):
     #     sel
+
+
+class TheQListWidget(QW.QListWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        # self.
+        # self.currentRowChanged
 
 
 class SpecData(object):
@@ -160,23 +203,51 @@ class TheWindow(QW.QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.resize(800, 600)
+        # self.resize(800, 600)
         # self.showMaximized()
         self.cenWidget = QW.QWidget()
-        self.setting = SettingQWidget()
         self.spectra_plot = SpectraPlot()
+        self.exp_list = TheQListWidget()
+        self.setting = SettingQWidget()
         self.read_file = ReadFileQWidget()
         self.setCentralWidget(self.cenWidget)
         self.setWindowIcon(QIcon("matplotlib_large.png"))
 
+        _layout2 = QW.QVBoxLayout()
+        _layout2.addWidget(self.exp_list)
+        _layout2.addWidget(self.setting)
         _layout1 = QW.QHBoxLayout()
         _layout1.addWidget(self.spectra_plot)
-        _layout1.addWidget(self.setting)
+        _layout1.addLayout(_layout2)
+        _layout1.addStretch(1)
         _layout0 = QW.QVBoxLayout()
         _layout0.addLayout(_layout1)
         _layout0.addWidget(self.read_file)
         _layout0.addStretch(1)
         self.cenWidget.setLayout(_layout0)
+        self._set_connect()
+
+    def _set_connect(self):
+        self.read_file.pathChanged.connect(self.read_spec_data)
+
+        def exp_list_row_changed_callback():
+            self.spectra_plot.highlight_line(self.exp_list.currentRow())
+
+        self.exp_list.currentRowChanged.connect(exp_list_row_changed_callback)
+
+    def read_spec_data(self):
+        # .asc file appends setting info.
+        output = np.genfromtxt(self.read_file.path, skip_footer=41)
+        wavelength = output[:, 0]
+        intensity = output[:, 1]
+        self.spectra_plot.add_spec(wavelength, intensity)
+        _pixmap = QPixmap(20, 20)
+        _current_line_color = self.spectra_plot._lines[-1].get_color()
+        _pixmap.fill(QColor(_current_line_color))
+        item_to_add = QW.QListWidgetItem()
+        item_to_add.setText(self.read_file._entry.text())
+        item_to_add.setIcon(QIcon(_pixmap))
+        self.exp_list.addItem(item_to_add)
 
 
 # ----------------------------------------------------------------------------------------------- #
@@ -187,7 +258,4 @@ else:
 app.setStyle(QW.QStyleFactory.create("Fusion"))
 window = TheWindow()
 window.show()
-window.spectra_plot.add_spec([1, 2, 3, 4], [2, 3, 4, 5])
-window.spectra_plot.add_spec([2, 3, 4, 5], [9, 8, 3, 4])
-# app.exec_()
 app.aboutToQuit.connect(app.deleteLater)
